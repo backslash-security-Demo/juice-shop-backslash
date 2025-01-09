7717d954ccb57ae3bcbf35ef58885a6ddd001c1d
mustafanaa <mustafa@backslash.security>
2025-01-09 16:22:07 +0200

    Cross file triggerable indirect package

--- routes/web3Wallet.ts
+++ routes/web3Wallet.ts
@@ -1,32 +1,22 @@
-import { type Request, type Response } from 'express'
-import challengeUtils = require('../lib/challengeUtils')
-import * as utils from '../lib/utils'
-const web3WalletABI = require('../data/static/contractABIs').web3WalletABI
-const ethers = require('ethers')
-const challenges = require('../data/datacache').challenges
-const web3WalletAddress = '0x413744D59d31AFDC2889aeE602636177805Bd7b0'
-const walletsConnected = new Set()
-let isEventListenerCreated = false
+import { Request, Response } from 'express';
+import { contractExploitHandler } from '../lib/contractHandler'
 
-module.exports.contractExploitListener = function contractExploitListener () {
-  return (req: Request, res: Response) => {
-    const metamaskAddress = req.body.walletAddress
-    walletsConnected.add(metamaskAddress)
-    try {
-      const provider = new ethers.WebSocketProvider('wss://eth-sepolia.g.alchemy.com/v2/FZDapFZSs1l6yhHW4VnQqsi18qSd-3GJ')
-      const contract = new ethers.Contract(web3WalletAddress, web3WalletABI, provider)
-      if (!isEventListenerCreated) {
-        contract.on('ContractExploited', (exploiter: string) => {
-          if (walletsConnected.has(exploiter)) {
-            walletsConnected.delete(exploiter)
-            challengeUtils.solveIf(challenges.web3WalletChallenge, () => true)
-          }
-        })
-        isEventListenerCreated = true
-      }
-      res.status(200).json({ success: true, message: 'Event Listener Created' })
-    } catch (error) {
-      res.status(500).json(utils.getErrorMessage(error))
+/**
+ * Middleware to handle requests and invoke the contract exploit handler.
+ */
+export const contractExploitListener = () => {
+  return async (req: Request, res: Response) => {
+    const metamaskAddress = req.body.walletAddress;
+
+    if (!metamaskAddress) {
+      return res.status(400).json({ success: false, message: 'Wallet address is required' });
     }
-  }
-}
+
+    try {
+      const result = await contractExploitHandler(metamaskAddress);
+      res.status(200).json({ success: true, message: result });
+    } catch (error) {
+      res.status(500).json({ success: false, message: error });
+    }
+  };
+};
